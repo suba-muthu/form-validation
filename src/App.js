0233c4494e2cdf10857a1463a36a2d42be36e3b1
suba <subalakshmimuthu02@gmail.com>
2023-03-09 12:58:25 +0530

    form validation in react.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const initialValues = {
+    userName: "",
+    email: "",
+    password: ""
+  }
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formError, setFormError] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({...formValues, [name]: value});
+    console.log(formValues);
+    // setIsSubmit(true);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError(validate(formValues));
+    setIsSubmit(true);
+  }
+
+  useEffect(() => {
+    console.log(formError);
+    if (Object.keys(formError).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formError]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex =/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.userName) {
+      errors.userName = "UserName is required!";
+    } else if (!values.userName.match(/^[a-zA-Z]+$/)) {
+      errors.userName = "only characters Required!"
+    }
+    if (!values.email) {
+      errors.email = "Email is Required!"
+    } else if (!regex.test(values.email)) {
+      errors.email ="This is not a valid email format!";
+    } 
+    if (!values.password) {
+      errors.password = "password is Required!"
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters!";
+    } else if (values.password.length > 10) {
+      errors.password = "password cannot exceed more than 10 characters!";
+    }
+    return errors;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {Object.keys(formError).length === 0 && isSubmit ? (
+        <div className='signup'>Signed in Successfully!</div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className='ui divider'></div>
+        <div className='ui form'>
+          <div className='field'>
+            <label>UserName</label>
+            <input 
+              type="text" 
+              name="userName" 
+              placeholder='UserName'
+              value={formValues.userName}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formError.userName}</p>
+          <div className='field'>
+            <label>Email</label>
+            <input 
+              type="text" 
+              name="email" 
+              placeholder='Email'
+              value={formValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formError.email}</p>
+          <div>
+            <label>Password</label>
+            <input 
+              type="text" 
+              name="password" 
+              placeholder='Password'
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formError.password}</p>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
